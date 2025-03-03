f182c7e0730e6464ea9572f3de2fa222c8521548
Hugo Chiarel <hchiarel@hotmail.fr>
2025-03-03 15:52:13 +0100

    modif ex5

--- cours C/TP1/src/ex5-division.c
+++ cours C/TP1/src/ex5-division.c
@@ -5,19 +5,22 @@ int main() {
     float flottant;
 
     entier = 10 / 3;
-    printf("10 / 3 en entier      : %d\n", entier);
+    printf("10 / 3 en entier : %d\n", entier);
+
+    flottant = 10 / 3;
+    printf("10/3 en float : %f\n", flottant);
 
     flottant = 10.0 / 3;
-    printf("10.0 / 3 en float     : %f\n", flottant);
+    printf("10.0 / 3 en float : %f\n", flottant);
 
     flottant = 10.0 / 3.0;
-    printf("10.0 / 3.0 en float   : %f\n", flottant);
+    printf("10.0 / 3.0 en float : %f\n", flottant);
 
     flottant = (float) 10 / 3;
-    printf("(float) 10 / 3       : %f\n", flottant);
+    printf("(float) 10 / 3 : %f\n", flottant);
 
     flottant = ((float) 10) / 3;
-    printf("((float) 10) / 3     : %f\n", flottant);
+    printf("((float) 10) / 3 : %f\n", flottant);
 
     flottant = ((float) 10) / 3;
     printf("((float) 10) / 3 (1 chiffre après virgule) : %.1f\n", flottant);
@@ -25,3 +28,7 @@ int main() {
 
     return 0;
 }
+
+printf("Si les deux éléments divisés sont des entiers, le reste de la division est ignoré");
+printf("\n10.0 est un float, donc 3 est converti en float avant la division.");
+printf("\n(float) 10 transforme 10 en 10.0, donc la division est flottante.");
