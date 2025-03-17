42b55ac90194e124d7869783565c502a96c591a8
Hugo Chiarel <hchiarel@hotmail.fr>
2025-03-17 12:36:00 +0100

    TP5 ex4

---
+++ cours C/TP5/src/ex4-1.c
@@ -0,0 +1,29 @@
+#define N 3
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct maillon {
+    int x;
+    struct maillon *suiv;
+} maillon;
+
+int main(void) {
+    maillon lc;
+    lc.x = 1;
+    printf("Valeur du champ x = %d\n\n", lc.x);
+
+    lc.suiv = (maillon *) malloc(sizeof(maillon));  // Allocation d'un deuxième maillon
+    if (lc.suiv == NULL) {  // Vérification de malloc
+        printf("Erreur d'allocation mémoire\n");
+        return 1;
+    }
+
+    lc.suiv->x = 2;
+    lc.suiv->suiv = NULL;  // Initialiser le pointeur suivant
+
+    printf("Valeur du champ x du deuxième maillon = %d\n\n", lc.suiv->x);
+
+    free(lc.suiv);  // Libération de la mémoire allouée
+
+    return 0;
+}
