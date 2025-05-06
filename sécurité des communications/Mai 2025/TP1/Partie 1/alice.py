4974bdb3db0c1e3a9c1fed8a891fe1c27a926121
Axel <axelmail22@gmail.com>
2025-05-06 15:47:52 +0200

    FIN P1

--- sécurité des communications/Mai 2025/TP1/Partie 1/alice.py
+++ sécurité des communications/Mai 2025/TP1/Partie 1/alice.py
@@ -3,6 +3,8 @@ from sympy import randprime
 from Cryptodome.Random import get_random_bytes
 from Cryptodome.Util import number
 import random
+import hashlib
+import json
 
 # Création du socket serveur
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,12 +17,14 @@ conn, addr = server.accept()
 print(f"[Alice] Bob connecté depuis {addr}")
 
 # --- Le reste du programme ---
-p = number.getPrime(2048) # p est un nombre premier
+p = number.getPrime(128) # p est un nombre premier
 a = random.randint(2, p-1) # a est un nombre aléatoire
 g = 2
 A_pkey = pow(g, a, p) # calcul de g**a%p
 
 # Envoi de la clé publique A à Bob
+conn.send(str(p).encode())
+print(f"[Alice] Nombre premier envoyé : {p}")
 conn.send(str(A_pkey).encode())
 print(f"[Alice] Clé publique envoyée : {A_pkey}")
 
@@ -33,10 +37,38 @@ AB_key = pow(B_pkey, a, p) # calcul B_pkey**a%p
 print(f"[Alice] Clé partagée : {AB_key}")
 
 # Génération d'une clé AES
-key = get_random_bytes(16)  # 128 bits
-print(f"[Alice] Clé AES générée : {key}")
-shared_key = key^AB_key.to_bytes(16, 'big')  # XOR de la clé AES avec la clé partagée
-print(f"[Alice] Clé partagée chiffré avec Bob : {shared_key}")
+aes_key = get_random_bytes(16)  # 128 bits
+print(f"[Alice] Clé AES générée : {aes_key}")
+
+AB_key_bytes = AB_key.to_bytes(16, 'big')
+
+# XOR octet par octet
+aes_shared_key = b''
+# On boucle sur les octets de la clé AES et de la clé partagée
+for a, b in zip(aes_key, AB_key_bytes):
+    aes_shared_key += bytes([a ^ b])
+
+print(f"[Alice] Clé partagée chiffrée avec Bob : {aes_shared_key}")
+
+sha256_hash = hashlib.sha256(aes_key).hexdigest()
+print(f"[Alice] Hash SHA-256 de la clé AES : {sha256_hash}")
+keys = {
+    'aes_shared_key': aes_shared_key.hex(),
+    'hash': sha256_hash
+}
+# Conversion du dictionnaire en JSON
+keys_json = json.dumps(keys)
+
+# Envoi du dictionnaire converti en JSON (en bytes)
+conn.send(keys_json.encode())
+
+ack_alice = conn.recv(1024)
+ack_decrypted = b''
+for a, b in zip(ack_alice, aes_key):
+    ack_decrypted += bytes([a ^ b])
+
+message = ack_decrypted.decode('utf-8')  # Là tu peux faire decode
+print(f"[Alice] Message de Bob : {message}")
 
 input("[Alice] Appuyez sur Entrée pour fermer la connexion...")
 print("[Alice] Connexion fermée.")
--- sécurité des communications/Mai 2025/TP1/Partie 1/bob.py
+++ sécurité des communications/Mai 2025/TP1/Partie 1/bob.py
@@ -2,7 +2,8 @@ import socket
 from sympy import randprime
 from Cryptodome.Util import number
 import random
-
+import json
+import hashlib
 
 # Création du socket client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,7 +24,7 @@ print(f"[Bob] Adresse locale : {local_ip}:{local_port}")
 print(f"[Bob] Alice est connectée sur : {remote_ip}:{remote_port}")
 
 # --- Le reste du programme ---
-p = number.getPrime(2048) # p est un nombre premier
+p = int(client.recv(1024).decode()) # p est un nombre premier reçu d'Alice
 b = random.randint(2, p-1) # a est un nombre aléatoire
 g = 2
 B_pkey = pow(g, b, p) # calcul de  g**b%p 
@@ -40,6 +41,39 @@ print(f"[Bob] Clé publique envoyée : {B_pkey}")
 AB_key = pow(A_pkey, b, p) # calcul A_pkey**b%p
 print(f"[Bob] Clé partagée : {AB_key}")
 
+receive_keys = client.recv(1024)
+print(f"[Bob] Clé partagée chiffrée avec Alice : {receive_keys}")
+
+keys = json.loads(receive_keys)
+
+# Récupérer la clé partagée chiffrée et le hash de la clé AES
+aes_shared_key_received = bytes.fromhex(keys['aes_shared_key'])  # Convertir la clé reçue en bytes
+
+AB_key_bytes = AB_key.to_bytes(16, 'big') # On convertit la clé partagée en bytes
+
+aes_key = b''
+for a, b in zip(aes_shared_key_received, AB_key_bytes):
+    aes_key += bytes([a ^ b])
+    
+print(f"[Bob] Clé aes : {aes_key}")
+received_hash = keys['hash']
+
+# Calculer le hash SHA-256 de la clé partagée chiffrée reçue
+calculated_hash = hashlib.sha256(aes_key).hexdigest()
+
+# Vérifier l'intégrité
+if calculated_hash == received_hash:
+    print("[Bob] L'intégrité de la clé chiffrée a été vérifiée avec succès.")
+    # Message de confirmation
+    message = "Bien reçu !"
+    message_bytes = message.encode('utf-8') # Convertir le message en bytes
+    # chiffrer le message avec la clé AES
+    aes_ack= b''
+    for a, b in zip(aes_key, message_bytes):
+        aes_ack += bytes([a ^ b])
+    client.send(aes_ack)
+else:
+    print("[Bob] ERREUR : L'intégrité de la clé chiffrée a échoué. La clé pourrait avoir été modifiée.")
 
 input("[Bob] Appuyez sur Entrée pour fermer la connexion...")
 client.close()
